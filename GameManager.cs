d0d5e132a9ebb6fc78a8c11a32a2f9b061f8e22d
JonnyRoth2 <115678019+JonnyRoth2@users.noreply.github.com>
2023-01-24 20:41:12 -0500

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int money;
+
+    public TextMeshProUGUI moneyText;
+
+    public static GameManager instance;
+
+    void Awake()
+        {
+            instance=this;
+        }
+    public void AddMoney(int amount)
+    {
+            money+=amount;
+            moneyText.text="Coins:"+money.ToString();
+
+    }
+    public void TakeMoney(int amount)
+    {
+            money-=amount;
+            moneyText.text="Coins:"+money.ToString();
+    }
+
+    
+}
